c6a2977577761a303e7b14d6d413907b53f067d3
Alex Williams <ascwnyc@gmail.com>
2022-08-14 16:10:58 +0100

    Incorporated GitHub API to read out latest release

--- commands/Utility/stats.js
+++ commands/Utility/stats.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
+const fetch = require('node-fetch');
 
 module.exports = {
     name: 'stats',
@@ -11,7 +12,7 @@ module.exports = {
         .setDescription("Shows a breakdown of Swift's statistics."),
     async execute({ client, interaction }) {;
         
-
+        // Initialise uptime units.
         let totalSeconds = (client.uptime / 1000);
         let hours = Math.floor(totalSeconds / 3600);
         totalSeconds %= 3600;
@@ -20,14 +21,17 @@ module.exports = {
         let seconds = Math.floor(totalSeconds % 60);
         let uptime = `${hours}H, ${minutes}m, ${seconds}s`;
 
+        // Fetch latest version from Swift's GitHub repo.
+        const response = await fetch('https://api.github.com/repos/ascwnyc/Swift/releases/latest');
+        const data = await response.json();
+
         const embed = new EmbedBuilder()
         .setColor('#5866EF')
         .setAuthor({name: `Swift Stats`, iconURL: client.user.avatarURL()})
         .addFields(
-            { name: ':desktop: Servers', value: '```' + `${client.guilds.cache.size}` +'```' },
-            { name: ':battery: Usage', value: '```20.6MB```', inline: true},
-            { name: ':hourglass: Uptime', value: '```' + uptime + '```', inline: true},
-            { name: ':rocket: Version', value: '```1.0.0```', inline: true}
+            { name: ':desktop: Servers', value: `\`\`\`${client.guilds.cache.size}\`\`\`` , inline: true},
+            { name: ':hourglass: Uptime', value: `\`\`\`${uptime}\`\`\`` , inline: true},
+            { name: ':rocket: Version', value: `\`\`\`${data.tag_name}\`\`\``, inline: true}
         );
 
         await interaction.reply({ embeds: [embed] });
