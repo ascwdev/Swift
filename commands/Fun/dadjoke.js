56bef2ee2abba9312da7697cda44709a100b5c6b
Alex Williams <ascwnyc@gmail.com>
2022-08-14 21:12:32 +0100

    Added dadjoke.js that tells bad jokes.

---
+++ commands/Fun/dadjoke.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+    description: 'Replies with a dad joke. They\'re terrible.',
+    usage: '`/dadjoke`',
+    permissions: '`none`',
+    data: new SlashCommandBuilder()
+        .setName('dadjoke')
+        .setDescription('Replies with a dad joke. They\'re terrible.'),
+    async execute({ interaction }) {
+
+        const response = await fetch(`https://icanhazdadjoke.com`, {
+            method: "GET",
+            headers: { Accept: "application/json" }
+        });
+        const data = await response.json();
+        
+
+        const embed = new EmbedBuilder()
+            .setColor('#5866EF')
+            .setAuthor({name: `Dad Joke`, iconURL: `https://i.imgur.com/zqhNpO1.png`})
+            .setDescription(data.joke);
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
